136d0c766187f2e7dcfe9f01d9e9fe9517555dc3
rbgus <rbgus>
2019-07-31 11:47:05 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rbgus\desktop\asf\likelion7_regularsession-master\6weeks\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
